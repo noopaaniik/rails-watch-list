9c70cd346c4e946f70c647e3243c7cbeabfcc65c
Bora Kim <miromiro@posteo.de>
2022-05-12 17:03:06 +0900

    poor style

---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :list
+  belongs_to :movie
+  validates :movie_id, uniqueness: { scope: :list_id }
+  validates :comment, presence: true, length: { minimum: 6 }, allow_blank: true
+end
